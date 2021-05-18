6248dc575ace00a639310aedb4182f315965b69b
ap7 <arhouston7@gmail.com>
2021-05-18 19:02:13 +0200

    View model tutorial

---
+++ app/src/main/java/fr/ap7/viewmodeldemo/ui/main/MainFragment.kt
@@ -0,0 +1,42 @@
+package fr.ap7.viewmodeldemo.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.ap7.viewmodeldemo.R
+import kotlinx.android.synthetic.main.main_fragment.*
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        resultText.text = viewModel.getResult().toString()
+
+        convertButton.setOnClickListener {
+            if (resultText.text.isNotEmpty()) {
+                viewModel.setAmount(dollarText.text.toString())
+                resultText.text = viewModel.getResult().toString()
+            } else {
+                resultText.text = R.string.no_value.toString()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/fr/ap7/viewmodeldemo/ui/main/MainViewModel.kt
@@ -0,0 +1,22 @@
+package fr.ap7.viewmodeldemo.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val usdToEuRate = 0.4f
+    private var dollarText = ""
+    private var result = 0f
+
+    fun setAmount(value: String) {
+        try {
+            dollarText = value
+            result = value.toFloat() * usdToEuRate
+        } catch (e: Exception) {
+            print(e.message)
+        }
+    }
+
+    fun getResult(): Float {
+        return result
+    }
+}
